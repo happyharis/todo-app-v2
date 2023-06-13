bbcd28f0bcd831875d35a72fcfd71070321bde78
Haris Samingan <muhamad_haris@hotmail.com>
2023-06-13 20:55:17 +0800

    Add timer

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,8 +1,31 @@
-import { Card } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Button, Card } from "react-bootstrap";
 
 export default function TodoCard({ todo }) {
   const completed = todo.completed;
   const border = completed ? "success" : "danger";
+  const [timer, setTimer] = useState(0);
+  const [timerInterval, setTimerInterval] = useState(null);
+
+  const startTimer = () => {
+    if (timerInterval === null) {
+      const intervalID = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 1);
+      }, 1000);
+      setTimerInterval(intervalID);
+    }
+  };
+
+  const pauseTimer = () => {
+    clearInterval(timerInterval);
+    setTimerInterval(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      clearInterval(timerInterval);
+    };
+  }, [timerInterval]);
   return (
     <>
       <Card border={border} className="my-3">
@@ -10,6 +33,13 @@ export default function TodoCard({ todo }) {
         <Card.Body>
           <Card.Title>{todo.title}</Card.Title>
           <Card.Text>{todo.description}</Card.Text>
+          <p>Timer: {timer} seconds</p>
+          <Button onClick={startTimer}>
+            <i className="bi bi-play"></i>
+          </Button>
+          <Button onClick={pauseTimer} className="mx-2">
+            <i className="bi bi-pause-fill"></i>
+          </Button>
         </Card.Body>
       </Card>
     </>
